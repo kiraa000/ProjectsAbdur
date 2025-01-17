bacc3b24cea13e2868c4cd5b0a0f9761684bbc45
kiraa000 <124541772+kiraa000@users.noreply.github.com>
2025-01-16 20:12:54 -0500

    Add files via upload

---
+++ github stuff/QuickSort.java
@@ -0,0 +1,58 @@
+public class QuickSort
+{
+  // Sorts a[0], ..., a[size-1] in ascending order
+  //   using the Quicksort algorithm.
+  public static void sort(double[] a)
+  {
+    recursiveSort(a, 0, a.length - 1);
+  }
+
+  // Recursive helper method: sorts a[from], ..., a[to]
+  private static void recursiveSort(double[] a, int from, int to)
+  {
+    if (from >= to)
+      return;
+
+    // Choose pivot a[p]:
+    int p = (from + to ) / 2;
+     
+
+    // Partition:
+    int i = from;
+    int j = to;
+    while (i <= j)
+    {
+      if (a[i] <= a[p])
+        i++;
+      else if (a[j] >= a[p])
+        j--;
+      else
+      {
+        swap (a, i, j);
+        i++;
+        j--;
+      }
+    }
+
+    // Finish partitioning:
+    if (p < j)    // place the pivot in its correct position
+    {
+      swap (a, j, p);
+      p = j;
+    }
+    else if (p > i)
+    {
+      swap (a, i, p);
+      p = i;
+    }
+
+    // Sort recursively:
+    recursiveSort(a, from, p - 1);
+    recursiveSort(a, p + 1, to);
+  }
+
+  private static void swap (double[] a, int i, int j)
+  {
+    double temp = a[i]; a[i] = a[j]; a[j] = temp;
+  }
+}
